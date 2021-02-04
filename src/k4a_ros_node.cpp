23f1145456dbb0b25f6bea754d209dda6ec56373
bart holmberg <bart.holmberg@gmail.com>
2021-02-03 18:52:53 -0800

    daily checkin - functioning
    daily checkin - functioning

--- src/k4a_ros_node.cpp
+++ src/k4a_ros_node.cpp
@@ -410,9 +410,9 @@ int main(int argc, char **argv)
     marker.pose.orientation.w = 1.0;
   
        // Set the scale of the marker -- 1x1x1 here means 1m on a side
-    marker.scale.x = 1.0;
-    marker.scale.y = 1.0;
-    marker.scale.z = 1.0;
+    marker.scale.x = 50.0;
+    marker.scale.y = 50.0;
+    marker.scale.z = 50.0;
   
        // Set the color -- be sure to set alpha to something non-zero!
     marker.color.r = 0.0f;
